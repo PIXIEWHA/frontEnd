e29ac3441e93495b44883d1b531407088a5fae5b
yhyeonjg <yhyeonjg@naver.com>
2022-12-10 19:39:09 +0900

    update front

--- django/App/apis/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class ApisConfig(AppConfig):
-    name = 'apis'
--- django/App/apis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ListUser, ListRb, ListReport
-
-urlpatterns = [
-    path('user/', ListUser.as_view()),
-    path('rbpi/', ListRb.as_view()),
-    path('report/', ListReport.as_view()),
-]
